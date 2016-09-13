71f0fa52b438f755bd742794e2038941e8981e47
Milos Milosevic <Milos Milosevic>
2016-09-13 18:04:29 +0200

    Initial Commit

---
+++ js/hover.js
@@ -0,0 +1,51 @@
+
+	function openNav() {
+	    document.getElementById("myNav").style.width = "100%";
+	}
+
+	function closeNav() {
+	    document.getElementById("myNav").style.width = "0%";
+	}
+
+$( ".gallery-items1" )
+		 .on("mouseenter", function() {
+		  $(".img-title1").show(100);
+		})
+		.on("mouseleave", function() {
+		  $(".img-title1").hide(100);
+		});
+	$( ".gallery-items2" )
+		 .on("mouseenter", function() {
+		  $(".img-title2").show(100);
+		})
+		.on("mouseleave", function() {
+		  $(".img-title2").hide(100);
+		});
+	$( ".gallery-items3" )
+		 .on("mouseenter", function() {
+		  $(".img-title3").show(100);
+		})
+		.on("mouseleave", function() {
+		  $(".img-title3").hide(100);
+		});
+	$( ".gallery-items4" )
+		 .on("mouseenter", function() {
+		  $(".img-title4").show(100);
+		})
+		.on("mouseleave", function() {
+		  $(".img-title4").hide(100);
+		});
+	$( ".gallery-items5" )
+		 .on("mouseenter", function() {
+		  $(".img-title5").show(100);
+		})
+		.on("mouseleave", function() {
+		  $(".img-title5").hide(100);
+		});
+	$( ".gallery-items6" )
+		 .on("mouseenter", function() {
+		  $(".img-title6").show(100);
+		})
+		.on("mouseleave", function() {
+		  $(".img-title6").hide(100);
+		});
---
+++ js/toBottom.js
@@ -0,0 +1,11 @@
+
+
+$(".home").click(function() {
+    $('html, body').animate({
+        scrollTop: $(".main-top-wrap").offset().top
+    }, 1000);
+
+});
+
+
+
